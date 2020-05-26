9fb30bcebe465f6f3f7cfe383015c91ceae1320b
dp3tcv <65833350+dp3tcv@users.noreply.github.com>
2020-05-26 20:08:55 -0100

    Update generate-code.service.ts

--- src/app/generate-code/generate-code.service.ts
+++ src/app/generate-code/generate-code.service.ts
@@ -13,7 +13,7 @@ export class GenerateCodeService {
 	constructor(private readonly http: ApiService) {}
 
 	sendData(data: AuthorizationCodeCreateDto): Observable<string> {
-		console.log("xpto");
+		
 		return this.http.post<AuthorizationCodeResponseDto>(this.api, data).pipe(
 			map(result => result.authorizationCode),
 			map(authCode => authCode.match(/(.{3})/g).join(' '))
