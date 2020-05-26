b01acaf28cf530b47f27dbaa99dac661b6761d03
dp3tcv <65833350+dp3tcv@users.noreply.github.com>
2020-05-26 18:59:57 -0100

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,11 +7,11 @@ export const environment = {
 	showWarning: true,
 	host: 'http://172.18.50.35:84',
 	eiamSelfAdmin:
-		'http://172.18.50.35:8080',
+		'http://172.18.50.50:8080',
 	oidc: {
 		clientId: 'CovidCode-UI',
 		afterLoginPath: '/generate-code',
-		stsServer: 'http://172.18.50.35:8080/realms//cv-covid-code',
+		stsServer: 'http://172.18.50.35:8080/realms/cv-covid-code',
 		applicationUrl: 'http://172.18.50.35:4200/auth/login-feedback/',
 		post_logout_redirect_uri: 'http://172.18.50.35:4200/',
 		silentRenew: true,
