763cbce4292eed3403d55dae60093a1d1d03cb7a
dp3tcv <65833350+dp3tcv@users.noreply.github.com>
2020-08-14 14:28:50 -0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@ export const environment = {
 	showWarning: true,
 	host: 'http://covidcodeservice.uniteltmais.cv/',
 	eiamSelfAdmin:
-		'http://keycloak.uniteltmais.cv',
+		'http://keycloak.nanosmon.cv',
 	oidc: {
 		clientId: 'CovidCode-UI',
 		afterLoginPath: 'generate-code',
-		stsServer: 'http://keycloak.uniteltmais.cv/auth/realms/cv-covid-code',
+		stsServer: 'http://keycloak.nanosmon.cv/auth/realms/cv-covid-code',
 		applicationUrl: 'http://covidcodeui.nanosmon.cv/',
 		loginFeedback: 'auth/login-feedback/',
 		max_id_token_iat_offset_allowed_in_seconds: 40000,
