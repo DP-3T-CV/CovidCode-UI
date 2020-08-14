d9494d50581330549912d94664d6ffcdb915a1a1
dp3tcv <65833350+dp3tcv@users.noreply.github.com>
2020-08-13 23:44:06 -0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,14 @@ import {OIdC} from '../app/auth/open-id-config-service';
 export const environment = {
 	production: true,
 	showWarning: true,
-	host: 'http://covidcodeservice.uniteltmais.cv/',
+	host: 'http://covidcodeservice.nanosmon.cv/',
 	eiamSelfAdmin:
 		'http://keycloak.nanosmon.cv',
 	oidc: {
 		clientId: 'CovidCode-UI',
 		afterLoginPath: 'generate-code',
-		stsServer: 'http://keycloak.nanosmon.cv/auth/realms/cv-covid-code',
-		applicationUrl: 'http://covidcodeui.uniteltmais.cv/',
+		stsServer: 'http://keycloak.uniteltmais.cv/auth/realms/cv-covid-code',
+		applicationUrl: 'http://covidcodeui.nanosmon.cv/',
 		loginFeedback: 'auth/login-feedback/',
 		silentRenew: true,
 		useAutoLogin: false,
